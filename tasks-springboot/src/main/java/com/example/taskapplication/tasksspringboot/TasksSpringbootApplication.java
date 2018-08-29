f81d8678785b6fb209fb80fac4d0a660882dff46
gnteja <gn.teja1902@gmail.com>
2018-08-29 12:00:32 -0400

    inital commit

---
+++ tasks-springboot/src/main/java/com/example/taskapplication/tasksspringboot/TasksSpringbootApplication.java
@@ -0,0 +1,12 @@
+package com.example.taskapplication.tasksspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasksSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TasksSpringbootApplication.class, args);
+	}
+}
